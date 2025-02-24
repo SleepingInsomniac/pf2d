90ac46c3dbcc8ef4bae944d3c38868b3d8f3611f
Alex Clink <alexclink@gmail.com>
2025-02-23 21:51:05 -0500

    Use clockwise winding rule

--- src/pf2d/drawable/fill_spline.cr
+++ src/pf2d/drawable/fill_spline.cr
@@ -1,34 +1,52 @@
 module PF2d::Drawable(T)
-  @tbt = PF2d::ThreadedBinaryTree(Tuple(UInt16, PF2d::Vec2(Float64))).new { |(x1, _d1), (x2, _d2)| x1 < x2 ? -1 : 1 }
+  EPSILON = 1e-6
 
-  # Fills a spline by even-odd shape filling for each scan line
-  @[Experimental]
+  record Intercept, x : Float64, d : PF2d::Vec2(Float64)
+  @tbt = PF2d::ThreadedBinaryTree(Intercept).new { |v1, v2| v1.x > v2.x ? 1 : -1 }
+
+  # Fills a spline by the clockwise winding rule
+  @[Experimental("Edge cases are not covered")]
   def fill_spline(spline : QuadSpline, color)
-    rect = spline.closed_rect
-    # Get the full bounding box of the closed spline
+    fill_splines({spline}, color)
+  end
+
+  # :ditto:
+  @[Experimental("Edge cases are not covered")]
+  def fill_splines(splines : Enumerable(QuadSpline), color)
+    return if splines.empty?
+
+    # Get the full bounding box of the closed splines
+    rect = splines.reduce(splines.first.closed_rect) { |rect, spline| rect.merge(spline.closed_rect) }
+
     # Iterate through each y value from top to bottom
     rect.top.to_i.upto(rect.bottom.to_i) do |y|
       @tbt.clear
       # For each curve in the closed spline...
-      spline.closed_curves do |curve|
-        # Calculate the t value for the intersection at y in the curve
-        curve.horizontal_intersects(y) do |t|
-          # Get the x value of the intersection
-          x = curve.at(t).x.to_u16
-          # Get the direction of the tangent at the intersection
-          # This will be used later to determine if we're inside or outside the closed spline
-          dir = curve.tangent(t)
+      splines.each do |spline|
+        spline.closed_curves do |curve|
+          # Calculate the t value for the intersection at y in the curve
+          curve.horizontal_intersects(y) do |t|
+            # Get the x value of the intersection
+            x = curve.at(t).x
+            # Get the direction of the tangent at the intersection
+            # This will be used later to determine if we're inside or outside the closed spline
+            dir = curve.tangent(t)
 
-          # Insert the x intercept and direction into the threaded binary tree (sorted by x position)
-          @tbt << {x, dir}
+            # Insert the x intercept and direction into the threaded binary tree (sorted by x position)
+            @tbt << Intercept.new(x, dir)
+          end
         end
       end
 
-      drawing = true
+      winding = 0 # Count the number of times we cross the curve
 
-      @tbt.each_cons_pair do |(x1, d1), (x2, d2)|
-        scan_line(x1, y, x2 - x1, color) if drawing
-        drawing = !drawing
+      @tbt.each_cons_pair do |i1, i2|
+        winding += 1 if i1.d.y < -EPSILON
+        winding -= 1 if i1.d.y > EPSILON
+
+        if winding > 0
+          scan_line(i1.x, y, (i2.x - i1.x), color)
+        end
       end
     end
   end

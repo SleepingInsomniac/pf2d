45f80df95091b689cb9c9c71e5694541acfdd819
Alex Clink <alexclink@gmail.com>
2026-01-07 18:03:42 -0500

    Remove requirement for size (moved to Canvas)

--- src/pf2d/viewable.cr
+++ src/pf2d/viewable.cr
@@ -1,18 +1,34 @@
 module PF2d::Viewable(T)
-  abstract def size : PF2d::Vec2
-
-  def width
-    size.x
-  end
-
-  def height
-    size.y
-  end
-
   # To be implemented by the including class
-  abstract def get_point(x : Number, y : Number)
+  abstract def get_point?(x : Number, y : Number) : T?
 
-  def get_point(point : PF2d::Vec2)
+  def get_point?(point : PF2d::Vec2) : T?
+    get_point?(point.x, point.y)
+  end
+
+  def get_point(x : Number, y : Number) : T
+    point = get_point?(x, y)
+    raise IndexError.new("out of bounds") if point.nil?
+    point
+  end
+
+  def get_point(point : PF2d::Vec2) : T
     get_point(point.x, point.y)
+  end
+
+  def [](x, y)
+    get_point(x, y)
+  end
+
+  def [](point : PF2d::Vec2)
+    get_point(point)
+  end
+
+  def []?(x, y)
+    get_point?(x, y)
+  end
+
+  def []?(point : PF2d::Vec2)
+    get_point?(point)
   end
 end

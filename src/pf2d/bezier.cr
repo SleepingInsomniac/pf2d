c15ef9249f4c91775c3e977953f59fca83982330
Alex Clink <alexclink@gmail.com>
2026-01-08 16:11:31 -0500

    Add convenience macro

--- src/pf2d/bezier.cr
+++ src/pf2d/bezier.cr
@@ -1,5 +1,14 @@
 module PF2d
   module Bezier
+    macro [](*args)
+      {% if args.size == 3 %}
+        PF2d::Bezier::Quad.new({{args.splat}})
+      {% elsif args.size == 4 %}
+        PF2d::Bezier::Cubic.new({{args.splat}})
+      {% else %}
+        raise "Invalid number of arguments"
+      {% end %}
+    end
   end
 end
 

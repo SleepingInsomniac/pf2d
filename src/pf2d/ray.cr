7de41a487300e897c5ed3a1ce97f246c8cf34db7
Alex Clink <alexclink@gmail.com>
2026-01-18 22:58:28 -0500

    Additional methods

--- src/pf2d/ray.cr
+++ src/pf2d/ray.cr
@@ -1,5 +1,3 @@
-require "./vec"
-
 module PF2d
   # Represents an infinite line by position *pos* and direction *dir*
   # dir should be normalized
@@ -14,14 +12,18 @@ module PF2d
     def initialize(@pos, @dir)
     end
 
-    def project(x, y)
-      project(Vec[x, y])
-    end
-
+    # Return the projection of the point *p* onto this ray
+    # forms a right triangle with the ray and the line between p and the projection
     def project(p)
       (p - @pos).dot(@dir)
     end
 
+    #:ditto:
+    def project(x, y)
+      project(Vec[x, y])
+    end
+
+    # Return the point at distance *t* along the ray
     def at(t : Float)
       @pos + @dir * t
     end
@@ -38,6 +40,24 @@ module PF2d
       return nil unless t >= 0.0 && u >= 0.0
 
       @pos + @dir * t
+    end
+
+    def closest_point(p)
+      t = project(p)
+      t = 0.0 if t < 0.0
+      at(t)
+    end
+
+    def distance_to(p)
+      (p - closest_point(p)).magnitude
+    end
+
+    def normal
+      Ray[@pos, @dir.normal]
+    end
+
+    def to_line(t : Float64)
+      Line[@pos, @pos + @dir * t]
     end
   end
 end

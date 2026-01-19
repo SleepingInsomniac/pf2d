120cb87e0b28e47a355a81b7ef93ed123b1d4bd3
Alex Clink <alexclink@gmail.com>
2026-01-18 23:00:03 -0500

    Allow tri operations, update methods

--- src/pf2d/tri.cr
+++ src/pf2d/tri.cr
@@ -1,5 +1,9 @@
 module PF2d
   struct Tri(T)
+    macro [](*args)
+      PF2d::Tri.new({{args.splat}})
+    end
+
     property p1 : Vec2(T)
     property p2 : Vec2(T)
     property p3 : Vec2(T)
@@ -7,7 +11,14 @@ module PF2d
     def initialize(@p1 : Vec2(T), @p2 : Vec2(T), @p3 : Vec2(T))
     end
 
-    def barycentric(p : Vec2(Number))
+    {% for op in %w[* / // + - % **] %}
+      # Applies `{{op.id}}` to all component of this Vec with the corresponding component of *other*
+      def {{ op.id }}(vec : Vec2)
+        Tri[p1 {{op.id}} vec, p2 {{op.id}} vec, p3 {{op.id}} vec]
+      end
+    {% end %}
+
+    def barycentric?(p : Vec2(Number))
       v0 = p2 - p1
       v1 = p3 - p1
       v2 = p - p1
@@ -19,6 +30,10 @@ module PF2d
       v = v0.det(v2) / denom
 
       {1.0 - u - v, u, v}
+    end
+
+    def lines
+      {Line[p1, p2], Line[p2, p3], Line[p3, p1]}
     end
 
     def min_x
@@ -37,11 +52,13 @@ module PF2d
       {p1.y, p2.y, p3.y}.max
     end
 
+    # Maps points from self to *dest* with affine interpolation
+    # useful for sampling textures
     def map_points(dest : Tri(Number))
-      min_y.floor.to_i.upto(max_y.ceil.to_i) do |y|
-        min_x.floor.to_i.upto(max_x.ceil.to_i) do |x|
-          p = Vec[x + 0.5, y + 0.5]
-          w = barycentric(p)
+      min_y.to_i.upto(max_y.ceil.to_i) do |y|
+        min_x.to_i.upto(max_x.ceil.to_i) do |x|
+          p = Vec[x, y]
+          w = barycentric?(p)
 
           next if w.nil?
 
@@ -50,9 +67,9 @@ module PF2d
           next if w0 < -1e-9 || w1 < -1e-9 || w2 < -1e-9
 
           sp = dest.p1 * w0 + dest.p2 * w1 + dest.p3 * w2
-          dp = [x.to_f, y.to_f]
+          dp = Vec[x.to_f, y.to_f]
 
-          yield sp, dp
+          yield({sp, dp})
         end
       end
     end

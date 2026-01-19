8cc566411310098ad1dafa1ba707ebd71379caca
Alex Clink <alexclink@gmail.com>
2026-01-18 22:37:46 -0500

    List requires manually

--- src/pf2d.cr
+++ src/pf2d.cr
@@ -1,5 +1,27 @@
 module PF2d
   VERSION = {% `shards version`.chomp.stringify %}
+
+  def self.degrees_to_radians(degrees : Float)
+    degrees * Math::PI / 180.0
+  end
 end
 
-require "./pf2d/*"
+require "./pf2d/matrix"
+require "./pf2d/vec"
+require "./pf2d/transform"
+require "./pf2d/threaded_binary_tree"
+require "./pf2d/line"
+require "./pf2d/rect"
+require "./pf2d/circle"
+require "./pf2d/ray"
+require "./pf2d/bezier"
+require "./pf2d/tri"
+require "./pf2d/poly"
+require "./pf2d/quad"
+require "./pf2d/quad_spline"
+
+require "./pf2d/drawable"
+require "./pf2d/viewable"
+require "./pf2d/canvas"
+require "./pf2d/clip"
+require "./pf2d/grid"

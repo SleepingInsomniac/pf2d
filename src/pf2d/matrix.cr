32a5c475c219b74919cb2d087be41b4bfba6b959
Alex Clink <alexclink@gmail.com>
2026-01-24 19:32:39 -0500

    Switch to macro defined Matrix structs, use row major index

--- src/pf2d/matrix.cr
+++ src/pf2d/matrix.cr
@@ -1,91 +1,83 @@
 module PF2d
-  struct Matrix(T, S)
-    include Indexable::Mutable(T)
+  abstract struct Matrix(T, W, H)
+    macro define(width, height)
+      {% unless PF2d.has_constant?("Mat#{width}x#{height}") %}
+        struct ::PF2d::Mat{{width}}x{{height}}(T) < ::PF2d::Matrix(T, {{width}}, {{height}})
+          W = {{width}}
+          H = {{height}}
+          S = {{width * height}}
 
-    getter values : StaticArray(T, S)
-    getter width : UInt8
-    getter height : UInt8
+          getter data : StaticArray(T, S)
 
-    # Creates a new square `Matrix` with the given args.splat*
-    #
-    # ```
-    # m = Matrix[1, 2, 3, 4] # => Matrix(Int32, 4) 2x2 [1, 2, 3, 4]
-    # ```
-    macro [](*args)
-      # width and height are the isqrt of args.size
-      {% if args.size == 4 %}
-        PF2d::Matrix(typeof({{args.splat}}), 4).new(2, 2, StaticArray[{{args.splat}}])
-      {% elsif args.size == 9 %}
-        PF2d::Matrix(typeof({{args.splat}}), 9).new(3, 3, StaticArray[{{args.splat}}])
-      {% elsif args.size == 16 %}
-        PF2d::Matrix(typeof({{args.splat}}), 16).new(4, 4, StaticArray[{{args.splat}}])
-      {% else %}
-        raise "Cannot determine width and height of matrix with {{ args.size }} elements, " \
-              "please provide them explicitly Matrix(Int32, 16).new(4, 4, StaticArray[...])"
+          def initialize
+            @data = StaticArray(T, S).new(0)
+          end
+
+          def initialize(@data)
+          end
+        end
       {% end %}
     end
 
-    def initialize(@width : UInt8, @height : UInt8)
-      @values = StaticArray(T, S).new(T.new(0))
+    macro [](*args)
+      PF2d::Mat{{W}}x{{H}}.new(StaticArray[{{args.splat}}])
     end
 
-    def initialize(@width : UInt8, @height : UInt8, @values : StaticArray(T, S))
+    include Indexable::Mutable(T)
+
+    delegate :fill, :unsafe_fetch, :unsafe_put, :size, to: @data
+
+    def ==(other : self)
+      @data == other.data
     end
 
-    delegate :fill, to: @values
-
-    def index(col : Int, row : Int)
-      row * width + col
+    def width
+      W
     end
 
-    def size
-      S
+    def height
+      H
     end
 
-    def unsafe_fetch(index : Int)
-      @values.unsafe_fetch(index)
+    @[AlwaysInline]
+    def [](row : Int, col : Int)
+      @data[row * {{ W }} + col]
     end
 
-    def unsafe_put(index : Int, value : T)
-      @values.unsafe_put(index, value)
-    end
-
-    # Fetch a value at a specified *column* and *row*
-    def [](col : Int, row : Int)
-      unsafe_fetch(index(col, row))
-    end
-
-    # Put a value at a specified *column* and *row*
-    def []=(col : Int, row : Int, value : T)
-      unsafe_put(index(col, row), value)
-    end
-
-    # Tests the equality of two matricies
-    def ==(other : Matrix)
-      self.values == other.values
-    end
-
-    def *(other : Matrix)
-      result = Matrix(typeof(@values.unsafe_fetch(0) * other.values.unsafe_fetch(0)), S).new(width, height)
-      (0...height).each do |row|
-        (0...width).each do |col|
-          (0...width).each do |n|
-            result[col, row] = result[col, row] + self[n, row] * other[col, n]
-          end
-        end
-      end
-      result
+    @[AlwaysInline]
+    def []=(row : Int, col : Int, value : T)
+      @data[row * {{ H }} + col] = value
     end
 
     def to_s(io)
-      io << {{@type}} << ' ' << width << "x" << height << " ["
-      {% for i in 0...S %}
-        io << unsafe_fetch({{i}})
-        {% if i != S - 1 %}
-          io << ", "
+      max_len = max.to_s.size
+      io << {{@type.stringify}} << "[\n"
+      {% for r in 0...H %}
+        io << "  "
+        {% for c in 0...W %}
+          io << @data[{{ r * W + c }}].to_s.rjust(max_len) {% if c < W - 1 %} << ", " {% end %}
         {% end %}
+        {% if r < H - 1 %} io << "\n" {% end %}
       {% end %}
-      io << ']'
+      io << "\n]"
+    end
+
+    macro define_mul(w)
+      struct ::PF2d::Mat{{W}}x{{H}}(T)
+        def *(other : ::PF2d::Mat{{w}}x{{W}})
+          ::PF2d::Mat{{w}}x{{H}}.new(StaticArray[
+            {% for r in 0...H %}
+              {% for c in 0...w %}
+                (
+                  {% for n in 0...W %}
+                    @data[{{r * W + n}}] * other.data[{{n * w + c}}] {% if n < W - 1 %}+{% end %}
+                  {% end %}
+                ),
+              {% end %}
+            {% end %}
+          ])
+        end
+      end
     end
   end
 end

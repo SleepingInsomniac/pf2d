6e277cfc38c6940343671973f9cf60d10262b3cf
Alex Clink <alexclink@gmail.com>
2026-02-07 18:23:16 -0500

    Add ceil to vec

--- src/pf2d/vec.cr
+++ src/pf2d/vec.cr
@@ -209,9 +209,11 @@ module PF2d
         Vec{{i}}(Float64).new({% for arg in 0...i %} @{{vars[arg].id}}.to_f64.round(precision), {% end %})
       end
 
-      def floor
-        Vec[{{ vars[0...i].map { |v| "@#{v.id}.floor" }.join(", ").id }}]
-      end
+      {% for method in %w[floor ceil] %}
+        def {{ method.id }}
+          Vec[{{ vars[0...i].map { |v| "@#{v.id}.#{method.id}" }.join(", ").id }}]
+        end
+      {% end %}
 
       def to(type)
         Vec[{% for arg in 0...i %} type.new(@{{vars[arg].id}}), {% end %}]
